c0391d98cb855800894a7c6be058a72d1540f368
adamsmolik <smolda8@gmail.com>
2024-02-15 21:31:26 +0100

    update singles init

--- shared/src/commonMain/kotlin/com/adamsmolik/dailypulse/articles/di/ArticlesModule.kt
+++ shared/src/commonMain/kotlin/com/adamsmolik/dailypulse/articles/di/ArticlesModule.kt
@@ -5,12 +5,13 @@ import com.adamsmolik.dailypulse.articles.data.ArticlesRepository
 import com.adamsmolik.dailypulse.articles.data.ArticlesService
 import com.adamsmolik.dailypulse.articles.application.ArticlesUseCase
 import com.adamsmolik.dailypulse.articles.presentation.ArticlesViewModel
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val articlesModule = module {
-    single<ArticlesService> { ArticlesService(get()) }
-    single<ArticlesUseCase> { ArticlesUseCase(get()) }
-    single<ArticlesViewModel> { ArticlesViewModel(get()) }
-    single<ArticlesDataSource> { ArticlesDataSource(get()) }
-    single<ArticlesRepository> { ArticlesRepository(get(), get()) }
+    singleOf(::ArticlesService)
+    singleOf(::ArticlesUseCase)
+    singleOf(::ArticlesViewModel)
+    singleOf(::ArticlesDataSource)
+    singleOf(::ArticlesRepository)
 }
--- shared/src/commonMain/kotlin/com/adamsmolik/dailypulse/sources/di/SourcesModule.kt
+++ shared/src/commonMain/kotlin/com/adamsmolik/dailypulse/sources/di/SourcesModule.kt
@@ -5,12 +5,13 @@ import com.adamsmolik.dailypulse.sources.data.SourcesDataSource
 import com.adamsmolik.dailypulse.sources.data.SourcesRepository
 import com.adamsmolik.dailypulse.sources.data.SourcesService
 import com.adamsmolik.dailypulse.sources.presentation.SourcesViewModel
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val sourcesModule = module {
-    single<SourcesService> { SourcesService(get()) }
-    single<SourcesUseCase> { SourcesUseCase(get()) }
-    single<SourcesDataSource> { SourcesDataSource(get()) }
-    single<SourcesRepository> { SourcesRepository(get(), get()) }
-    single<SourcesViewModel> { SourcesViewModel(get()) }
+    singleOf(::SourcesService)
+    singleOf(::SourcesUseCase)
+    singleOf(::SourcesDataSource)
+    singleOf(::SourcesRepository)
+    singleOf(::SourcesViewModel)
 }
